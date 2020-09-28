13f759b14b24a03d0fe5f69415fe6bbe9f8d9195
Yoann Abran <y.abran@codeur.online>
2020-09-28 15:18:49 +0200

    fix bug dashview

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -7,6 +7,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Assets;
 use App\Entity\Book;
 use App\Entity\User;
 
@@ -19,7 +20,7 @@ class DashboardController extends AbstractDashboardController
      */
     public function index(): Response
     {
-        return parent::index();
+        return $this->render('admin/dashboard.html.twig');
     }
 
     public function configureDashboard(): Dashboard
